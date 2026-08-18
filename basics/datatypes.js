a492d6f8a8c9ad06fa7fcdd75b608ae53d05da68
Deepika <anjali8139958932@gmail.com>
2026-08-18 14:17:43 +0000

    add datatypes file

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+//numbers,strings,booleans,arrays,objects,undefined,null,symbols,bigints
+//null typeof null is object
+
+let state="karnataka"
+const age=25
+//number =>2^53-1
+//string =>"deepika"
+// boolean =>true/false
+// null =>standard value for empty or unknown
+//symbols =>unique and immutable
+//objects =>key value pairs
+console.log(typeof undefined);
+console.log(typeof null);
