56c68f124f540f341e4ceea39b5ec1a26a473685
Deepika <anjali8139958932@gmail.com>
2026-08-24 09:22:49 +0000

    typecoversion  done

---
+++ basics/TYPEcONVERSION.js
@@ -0,0 +1,27 @@
+let score="19abc";
+//to find a type
+console.log(typeof score);
+console.log(typeof(score));
+
+//type conversion
+ 
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+//o/p=NaN =>is a datatype,number not defined  ,"33abc"
+//"33"=>33
+//true=>1,false=>0
+let isLogged=1
+let booleanisLogged=Boolean(isLogged);
+console.log(booleanisLogged);
+
+//1=>true;0=>false
+//"=>false
+// "deepa"->true
+
+let num=44
+
+let num_2string=String(num)
+console.log(num_2string);
+console.log(typeof num_2string);
+
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -3,11 +3,21 @@
 
 let state="karnataka"
 const age=25
+//PRIMITIVE DATATYPW
 //number =>2^53-1
 //string =>"deepika"
 // boolean =>true/false
 // null =>standard value for empty or unknown
 //symbols =>unique and immutable
+
+//NON -PRIMITIGVE
 //objects =>key value pairs
 console.log(typeof undefined);
 console.log(typeof null);
+
+
+//"use strict" -to use modern java features
+//alert(3+3) we re using modejs not browseer
+
+let  name='deepika'//string typr
+let age =18// number
