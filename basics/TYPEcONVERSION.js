1c0a00c65555179c1f49500e1e15ac94aa66d8a4
Deepika <anjali8139958932@gmail.com>
2026-08-24 09:43:03 +0000

    typeconversion donr

--- basics/TYPEcONVERSION.js
+++ basics/TYPEcONVERSION.js
@@ -1,27 +1,68 @@
-let score="19abc";
-//to find a type
-console.log(typeof score);
-console.log(typeof(score));
+// let score="19abc";
+// //to find a type
+// console.log(typeof score);
+// console.log(typeof(score));
 
-//type conversion
+// //type conversion
  
-let valueInNumber=Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
-//o/p=NaN =>is a datatype,number not defined  ,"33abc"
-//"33"=>33
-//true=>1,false=>0
-let isLogged=1
-let booleanisLogged=Boolean(isLogged);
-console.log(booleanisLogged);
+// let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// //o/p=NaN =>is a datatype,number not defined  ,"33abc"
+// //"33"=>33
+// //true=>1,false=>0
+// let isLogged=1
+// let booleanisLogged=Boolean(isLogged);
+// console.log(booleanisLogged);
 
-//1=>true;0=>false
-//"=>false
-// "deepa"->true
+// //1=>true;0=>false
+// //"=>false
+// // "deepa"->true
 
-let num=44
+// let num=44
 
-let num_2string=String(num)
-console.log(num_2string);
-console.log(typeof num_2string);
+// let num_2string=String(num)
+// console.log(num_2string);
+// console.log(typeof num_2string);
+
+
+// //*************operations********************
+
+// let value = 3
+// let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+let str1="hello  "
+let str2="folks"
+
+let str3=str1+ str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+// do not do this even if it works becoxz for readability
+console.log("1" + "2"+2+2);
+console.log(1 +1+ 1+ "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=1;
+
+let gcount=3
+gcount++;
+console.log(gcount);
+
+
+
+
 
