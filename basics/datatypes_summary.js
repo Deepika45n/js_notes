86453af77accdb3dd7582c952680b3602c21d8d3
Deepika <anjali8139958932@gmail.com>
2026-08-25 13:07:33 +0000

    string explained

--- basics/datatypes_summary.js
+++ basics/datatypes_summary.js
@@ -26,6 +26,49 @@ const myfunc=function(){
 console.log(typeof heroes);
 console.log(anotherId);
 
+//primitive(stack-> u will get copy)
+//non-prmitive(refference)
+
+let str = "Hello";
+
+let str1=str;
+console.log(str1);
+str1="hello world";
+console.log(str);
+console.log(str1);
+
+let user={
+    name:"deepika",
+    age:20,
+}
+console.log(user.name);
+console.log(user.age);
+
+
+let user2=user
+user2.name="deepa"
+
+console.log(user2.name);
+console.log(user2.age);
+console.log(user.name);
+console.log(user.age);
+
+// deepika
+// 20
+// deepa
+// 20
+// deepa
+// 20  values changes in both obj becoz it refer to same memory
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+//istead of  console.log(str2 + phrase)
+
+console.log(`${str1} and  ${str2}`);//hello world and  Single quotes are ok too
+
+
+
+
 
 
 
